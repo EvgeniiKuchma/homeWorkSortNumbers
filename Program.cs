f78e52d5993b76c04030a5f9dee26f5f413e6da6
Евгений Кучма <desferto87@gmail.com>
2024-07-07 16:16:56 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace homeWorkSortNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int numberesArraySize = 10;
+            int minValue = 1;
+            int maxValue = 10;
+            int tempNumber = 0;
+
+            int[] numberesArray = new int[numberesArraySize];
+
+            for (int i = 0; i < numberesArraySize; i++)
+            {
+                numberesArray[i] = random.Next(minValue, maxValue);
+            }
+
+            for (int i = 0; i < numberesArraySize - 1; i++)
+            {
+                if (numberesArray[i] > numberesArray[i + 1])
+                {
+                    tempNumber = numberesArray[i];
+                    numberesArray[i + 1] = numberesArray[i];
+                    numberesArray[i] = tempNumber;
+                }
+
+                Console.Write(numberesArray[i] + ", ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
